aac2eb592ddcc9e4eae0502fae69c683c2c19d01
Rafaela Ranieri Lassen <lassenrafaela@gmail.com>
2024-12-09 16:15:52 -0300

    ClienteController

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
+
+import dao.ClienteDAO;
+import java.util.List;
+import model.Cliente;
 
 /**
  *
  * @author 44966
  */
 public class ClienteController {
+    ClienteDAO clienteDAO = new ClienteDAO();
+    public ClienteController(){
+        
+    }
     
+    public void cadastrar (String nome, String telefone, String endereco, String email){
+        Cliente cliente = new Cliente ();
+        cliente.setNome(nome);
+        cliente.setTelefone(telefone);
+        cliente.setEndereco (endereco);
+        cliente.setEmail (email);
+        clienteDAO.cadastrar(cliente);
+    }
+    
+    public List<Cliente> listar (String pesquisa){
+        return clienteDAO.listar(pesquisa);
+    }
+    
+    public void alterar ( int codigo, String nome, String telefone, String endereco, String email){
+        Cliente cliente = new Cliente ();
+        cliente.setNome(nome);
+        cliente.setTelefone(telefone);
+        cliente.setEndereco (endereco);
+        cliente.setEmail (email);
+        clienteDAO.alterar(cliente);
+    }
+    
+    public void excluir (int codigo){
+       clienteDAO.excluir(codigo);
+    }
+
 }
